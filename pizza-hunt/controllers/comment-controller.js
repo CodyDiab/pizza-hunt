898eeb73f1a5365b7fe7d40af9e7ac7a77fa9d84
Diab <emailcodydiab@gmail.com>
2020-07-13 17:00:20 -0500

    comment model

--- pizza-hunt/controllers/comment-controller.js
+++ pizza-hunt/controllers/comment-controller.js
@@ -1,10 +1,8 @@
-const {Comment, Pizza} = require('../modals');
+const {Comment, Pizza} = require('../models');
 
 const commentController = {
-    addComment() {
-
-    },
-    removeComment({ params, body }, res) {
+  
+    addComment({ params, body }, res) {
         console.log(body);
         Comment.create(body)
         .then(({_id})=> {
@@ -24,6 +22,28 @@ const commentController = {
         .catch(err => res.json(err));
 
     },
+    removeComment({ params}, res) {
+        Comment.findOneAndDelete({_id: params.commentId})
+        .then(deletedComment => {
+            if(!deletedComment) {
+            return res.status(404).json({message: 'No comment with this id'})
+            }
+            return Pizza.findOneAndUpdate(
+                {_id: params.pizzaId},
+                {$pull: { comments: params.commentId}},
+                {new: true}
+            );
+        })
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+                
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    }
 };
 
 module.exports = commentController;
--- pizza-hunt/models/Pizza.js
+++ pizza-hunt/models/Pizza.js
@@ -1,4 +1,6 @@
 const {Schema, model} = require('mongoose');
+const moment = require('moment');
+
 
 const PizzaSchema = new Schema({
     pizzaName: {
@@ -9,7 +11,8 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('mmm dd,yyyy [at] hh:mm a')
     },
     size: {
         type: String,
@@ -25,6 +28,7 @@ const PizzaSchema = new Schema({
 {
     toJSON: {
         virtuals: true,
+        getters: true
     },
     id: false
 }
